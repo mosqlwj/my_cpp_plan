bd8f8e20a24e3faf3138508c5fadd8ae84b98b87
lwj <2915655152@qq.com>
2021-07-12 19:08:20 +0800

    add 509

---
+++ leetcode.h
@@ -0,0 +1,9 @@
+//
+// Created by LWJ on 2021/7/12.
+//
+
+#ifndef MY_LEETCODE_PLAN_LEETCODE_H
+#define MY_LEETCODE_PLAN_LEETCODE_H
+#include <bits/stdc++.h>
+using namespace std;
+#endif //MY_LEETCODE_PLAN_LEETCODE_H

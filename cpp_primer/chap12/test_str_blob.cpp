2ebc861c58d46bbaabd4656d093f1dfd4095064e
lwj <2915655152@qq.com>
2021-07-13 17:41:23 +0800

    add cpp primer chaper12 strblob program

---
+++ cpp_primer/chap12/test_str_blob.cpp
@@ -0,0 +1,14 @@
+//
+// Created by LWJ on 2021/7/13.
+//
+#include "str_blob.h"
+
+int main()
+{
+    const StrBlob csb{ "hello", "world", "pezy" };
+    StrBlob sb{ "hello", "world", "Mooophy" };
+
+    std::cout << csb.front() << " " << csb.back() << std::endl;
+    sb.back() = "pezy";
+    std::cout << sb.front() << " " << sb.back() << std::endl;
+}

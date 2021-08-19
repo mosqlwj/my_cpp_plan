2dc209176b5945e79df3c1bb65256af038e84c59
lwj <2915655152@qq.com>
2021-08-19 20:06:45 +0800

    add 121

---
+++ dp/121_maxProfit.cpp
@@ -0,0 +1,19 @@
+//
+// Created by LWJ on 2021/8/19.
+//
+
+#include "../leetcode.h"
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<int> dp(n, 0);
+        int min_price = prices[0];
+        // 状态转移
+        for (int i = 1; i < n; i++) {
+            min_price = min(min_price, prices[i]);
+            dp[i] = max(dp[i - 1], prices[i] - min_price);
+        }
+        return dp[n - 1];
+    }
+};

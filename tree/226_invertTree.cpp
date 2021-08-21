ea8d436ef55116063574e7c62f99465ba7dff797
lwj <2915655152@qq.com>
2021-08-21 20:35:01 +0800

    add 114

--- tree/226_invertTree.cpp
+++ tree/226_invertTree.cpp
@@ -6,6 +6,7 @@
 
 class Solution {
 public:
+    // dfs
     TreeNode* invertTree(TreeNode* root) {
         if (root == nullptr)
             return nullptr;
@@ -14,6 +15,29 @@ public:
         root->right = tmp;
         invertTree(root->left);
         invertTree(root->right);
+        return root;
+    }
+    // bfs
+    TreeNode* invertTree2(TreeNode* root) {
+        if (root == nullptr)
+            return nullptr;
+        queue<TreeNode*> que;
+        que.push(root);
+
+        while (!que.empty()) {
+            TreeNode* node = que.front();
+            que.pop();
+            TreeNode* tmp = node->left;
+            node->left = node->right;
+            node->right = tmp;
+            if (node->left) {
+                que.push(node->left);
+            }
+            if (node->right) {
+                que.push(node->right);
+            }
+        }
+
         return root;
     }
 };

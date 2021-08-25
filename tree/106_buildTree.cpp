dbfb78f765ed30762099cf9508e31e557d720644
lwj <2915655152@qq.com>
2021-08-25 23:04:38 +0800

    add 106

---
+++ tree/106_buildTree.cpp
@@ -0,0 +1,32 @@
+//
+// Created by liwenjie on 2021/8/25.
+//
+#include "../leetcode.h"
+
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        return dfs(inorder, 0, inorder.size() - 1, postorder, 0, postorder.size() - 1);
+    }
+
+    TreeNode* dfs(vector<int>& inorder, int ib, int ie, vector<int>& postorder, int pb, int pe) {
+
+        // base case
+        if (ib > ie || pb > pe) {
+            return nullptr;
+        }
+        int index = 0;
+        int tmp = postorder[pe];
+        TreeNode* root = new TreeNode(tmp);
+        for (int i = ib; i <= ie; i++) {
+            if (inorder[i] == tmp) {
+                index = i;
+                break;
+            }
+        }
+        int count = index - ib;
+        root->left = dfs(inorder, ib, index - 1, postorder, pb, pb + count - 1);
+        root->right = dfs(inorder, index + 1, ie, postorder,  pb + count, pe - 1);
+        return root;
+    }
+};

879002a9fe8c55829faab5873ae0ff4f89793163
lwj <2915655152@qq.com>
2021-08-27 23:34:17 +0800

    fix 96

--- tree/96_numTrees.cpp
+++ tree/96_numTrees.cpp
@@ -1,11 +1,15 @@
 //
 // Created by LWJ on 2021/8/26.
 //
+#include "../leetcode.h"
 
 class Solution {
 public:
     int numTrees(int n) {
-        return dfs(1, n);
+//        return dfs(1, n);
+        vector<vector<int>> mem(n + 1, vector<int>(n + 1, 0));
+        int res = dp(1, n, mem);
+        return res;
     }
 
     // 闭区间[lo, hi]
@@ -25,4 +29,26 @@ public:
         }
         return res;
     }
+
+    int dp(int lo, int hi, vector<vector<int>>& mem) {
+        if (lo > hi)
+            return 1;
+        if (mem[lo][hi] != 0)
+            return mem[lo][hi];
+
+        int res = 0;
+        for (int i = lo; i <= hi; i++) {
+            int left = dp(lo, i - 1, mem);
+            int right = dp(i + 1, hi, mem);
+            res += left * right;
+        }
+        mem[lo][hi] = res;
+        return res;
+    }
 };
+
+int main() {
+    int res = Solution().numTrees(3);
+    cout << res << endl;
+    return 0;
+}

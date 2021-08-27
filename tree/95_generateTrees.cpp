b43ba7fac049ede0205a0491a949bc0bc3bb448a
lwj <2915655152@qq.com>
2021-08-27 23:53:02 +0800

    add 95

---
+++ tree/95_generateTrees.cpp
@@ -0,0 +1,39 @@
+//
+// Created by liwenjie on 2021/8/27.
+//
+#include "../leetcode.h"
+
+class Solution {
+public:
+    vector<TreeNode*> generateTrees(int n) {
+        vector<TreeNode*> res = dfs(1, n);
+        return res;
+    }
+
+    vector<TreeNode*> dfs(int lo, int hi) {
+        vector<TreeNode*> res;
+        if (lo > hi) {
+            res.push_back(nullptr);
+            return res;
+        }
+
+        for (int i = lo; i <= hi; i++) {
+            vector<TreeNode*> lefts = dfs(lo, i - 1);
+            vector<TreeNode*> rights = dfs(i + 1, hi);
+            for (auto& left : lefts) {
+                for (auto& right : rights) {
+                    TreeNode* root = new TreeNode(i);
+                    root->left = left;
+                    root->right = right;
+                    res.push_back(root);
+                }
+            }
+        }
+        return res;
+    }
+};
+
+int main() {
+    Solution().generateTrees(3);
+    return 0;
+}

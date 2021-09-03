8986b466ecf2f636d9fafe82d024ef7bec9d1449
lwj <2915655152@qq.com>
2021-09-03 19:59:30 +0800

    add 100 572

---
+++ tree/572_isSubtree.cpp
@@ -0,0 +1,28 @@
+//
+// Created by LWJ on 2021/9/3.
+//
+#include "../leetcode.h"
+class Solution {
+public:
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if (root == nullptr && subRoot == nullptr)
+            return true;
+        if (root == nullptr || subRoot == nullptr)
+            return false;
+        return isSameTree(root, subRoot) ||
+               isSubtree(root->left, subRoot) ||
+               isSubtree(root->right, subRoot);
+    }
+
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p == nullptr && q == nullptr)
+            return true;
+        if (p == nullptr || q == nullptr)
+            return false;
+        if (p->val != q->val)
+            return false;
+        bool left = isSameTree(p->left, q->left);
+        bool right = isSameTree(p->right, q->right);
+        return left && right;
+    }
+};

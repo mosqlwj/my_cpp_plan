01f745e9695ed44cd9476c59dc7d7727223792a7
lwj <2915655152@qq.com>
2021-09-07 21:17:18 +0800

    fix 257

--- tree/257_binaryTreePaths.cpp
+++ tree/257_binaryTreePaths.cpp
@@ -13,9 +13,15 @@ public:
         dfs(root, "", res);
         return res;
     }
-
+    // 这里传入string path的copy，每次递归是都是一份新的path副本
+    // 改进策略：使用回溯方法，传入string的引用，但是需要在递归后将path恢复之前的状态。
+    // 回溯其实就是在穷举。
     void dfs(TreeNode* root, string path, vector<string>& res) {
         // 此处root != nullptr
+        // 处理左子树或者右子树为空的情况
+        //    1
+        //  /   \
+        //  2   null
         if (root == nullptr)
             return;
         path += to_string(root->val);

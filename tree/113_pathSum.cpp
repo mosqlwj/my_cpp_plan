92d5bbf15a86db5910cc839ac4c1531262371f56
lwj <2915655152@qq.com>
2021-09-11 21:17:37 +0800

    add 113

---
+++ tree/113_pathSum.cpp
@@ -0,0 +1,41 @@
+//
+// Created by LWJ on 2021/9/11.
+//
+#include "../leetcode.h"
+
+
+class Solution {
+public:
+    // 回溯
+    void backtrack(TreeNode* root, int targetSum, vector<int>& path, vector<vector<int>>& res) {
+//        if (root == nullptr)
+//            return ;
+
+        if (root->left == nullptr && root->right == nullptr) {
+            path.push_back(root->val);
+            int sum = std::accumulate(path.begin(), path.end(), 0);
+            if (sum == targetSum) {
+                res.push_back(path);
+            }
+            return;
+        }
+
+        path.push_back(root->val);
+        if (root->left) {
+            backtrack(root->left, targetSum, path, res);
+            path.pop_back();
+        }
+        if (root->right) {
+            backtrack(root->right, targetSum, path, res);
+            path.pop_back();
+        }
+        return ;
+    }
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+        vector<vector<int>> res;
+        vector<int> path;
+        if (root == nullptr) return res;
+        backtrack(root, targetSum, path, res);
+        return res;
+    }
+};

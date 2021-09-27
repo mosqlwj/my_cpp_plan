b8c044805f17ed387e815e8db93ad48496a3d62f
lwj <2915655152@qq.com>
2021-09-27 23:23:54 +0800

    add 47

---
+++ backtrack/permute/47_permuteUnique.cpp
@@ -0,0 +1,27 @@
+#include "../../leetcode.h"
+
+// 对于有重复元素的数组，先进行排序，在去重处理
+
+class Solution {
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+
+        sort(nums.begin(), nums.end());
+    }
+
+    void backtrack(vector<int>& nums, 
+                   vector<bool>& used, 
+                   vector<int>& path, 
+                   vector<vector<int>>& res) {
+        
+        if (path.size() == nums.size()) {
+            res.push_back(path);
+            return;
+        }
+
+        for (int i = 0; i < nums.size(); i++) {
+            
+        }
+    
+    }
+};

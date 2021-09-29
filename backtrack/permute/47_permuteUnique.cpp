751c4cb37205706e9e23096448f9cc6f59c41381
lwj <2915655152@qq.com>
2021-09-29 21:50:37 +0800

    add 47

--- backtrack/permute/47_permuteUnique.cpp
+++ backtrack/permute/47_permuteUnique.cpp
@@ -5,8 +5,14 @@
 class Solution {
 public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-
+        vector<int> path;
+        vector<vector<int>> res;
+        if (nums.empty())
+            return res;
         sort(nums.begin(), nums.end());
+        vector<bool> used(nums.size(), false);
+        backtrack(nums, used, path, res);
+        return res;
     }
 
     void backtrack(vector<int>& nums, 
@@ -20,7 +26,17 @@ public:
         }
 
         for (int i = 0; i < nums.size(); i++) {
+            // 同一层上存在重复值，
+            if (i > 0 && nums[i] == nums[i - 1] && used[i - 1] == false)
+                continue;
+            if (used[i] == true)
+                continue;
             
+            used[i] = true;
+            path.push_back(nums[i]);
+            backtrack(nums, used, path, res);
+            path.pop_back();
+            used[i] = false;
         }
     
     }

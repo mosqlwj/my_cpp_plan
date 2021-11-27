684ec541128365242d424e858d1360f640b72bb4
lwj <2915655152@qq.com>
2021-11-27 22:59:48 +0800

    add Strblob

--- cpp_primer/chap15/main.cpp
+++ cpp_primer/chap15/main.cpp
@@ -1,9 +1,21 @@
+#include "StrBlob.h"
 #include <iostream>
 
 using namespace std;
 
+void Test_StrBlob() {
+
+    StrBlob sb{"hello", "nihao"};
+    // sb.push_back("nihao");
+    string res = sb.front();
+    cout << sb.front() << endl;
+
+}
+
+
 int main() {
 
     cout << "hello world" << endl;
+    Test_StrBlob();
     return 0;
 }

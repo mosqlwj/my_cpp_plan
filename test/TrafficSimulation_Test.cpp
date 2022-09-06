632d4b7eadf7b96f1de5770c1563485711741166
lwj <2915655152@qq.com>
2022-09-06 22:51:11 +0800

    init traffic simulation project

---
+++ test/TrafficSimulation_Test.cpp
@@ -0,0 +1,11 @@
+//
+// Created by lwj on 22-9-6.
+//
+
+#include "Demo.h"
+
+int main() {
+    Demo d;
+    d.Print();
+    return 0;
+}
---
+++ traffic_simulation/src/Demo.cpp
@@ -0,0 +1,8 @@
+//
+// Created by lwj on 22-9-6.
+//
+#include "Demo.h"
+#include <iostream>
+void Demo::Print() {
+    std::cout << "Hello Demo!" << std::endl;
+}

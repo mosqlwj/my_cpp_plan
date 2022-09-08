df0bb3eca956c7ae7a8a349390696d73a08aba1a
lwj <2915655152@qq.com>
2022-09-08 22:15:49 +0800

    init traffic simulation project with uint test

--- main.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by LWJ on 2021/7/12.
-//
-#include <iostream>
-using namespace std;
-int main() {
-    cout << "hello my cpp plan" << endl;
-    return 0;
-}
--- traffic_simulation/src/Demo.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by lwj on 22-9-6.
-//
-#include "Demo.h"
-#include <iostream>
-void Demo::Print() {
-    std::cout << "Hello Demo!" << std::endl;
-}
